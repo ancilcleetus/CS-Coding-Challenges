3d96cd4e27875308f0c9d3116a88c3c4afc3b98d
Ancil Cleetus <ancilcleetus.work@gmail.com>
2022-03-27 10:12:37 +0530

    Added Platform-01-AlgoDaily/0001-reverse-a-string.py

---
+++ Platform-01-AlgoDaily/0001-reverse-a-string.py
@@ -0,0 +1,38 @@
+# This is my solution for AlgoDaily problem Reverse a String
+# Located at https://algodaily.com/challenges/reverse-a-string
+
+def reverse_string(str1):
+    new_str = ""
+    
+    for idx in range(len(str1)-1, -1, -1):
+      new_str += str1[idx]
+      
+    return new_str
+
+
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test_1(self):
+        assert reverse_string("njnschnjkdasn j32 uida") == "adiu 23j nsadkjnhcsnjn"
+        print(
+            "PASSED: reverse_string('njnschnjkdasn j32 uida') should return 'adiu 23j nsadkjnhcsnjn'"
+        )
+
+    def test_2(self):
+        assert reverse_string("timbuktu12") == "21utkubmit"
+        print("PASSED: reverse_string('timbuktu12') should return '21utkubmit'")
+
+    def test_3(self):
+        assert reverse_string("") == ""
+        print("PASSED: reverse_string('') should return ''")
+
+    def test_4(self):
+        assert reverse_string("reverseastring") == "gnirtsaesrever"
+        print("PASSED: reverse_string('reverseastring') should return 'gnirtsaesrever'")
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
+    print("Nice job, 4/4 tests passed!")
